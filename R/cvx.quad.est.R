b9e9ff62cf15e6d4602e365c9cfe24056124f2ef
Kenta426 <kt426@cornell.edu>
2023-04-02 12:24:05 -0400

    added comment blocks for isotonic and convex regression

--- R/cvx.quad.est.R
+++ R/cvx.quad.est.R
@@ -1,12 +1,16 @@
-#' Title
+# Estimating twice differentiable function via convex regression +
+# quadratic function.
+
+#' Nonparametric LSE for twice differentiable function
 #'
-#' @param x
-#' @param y
-#' @param L
-#' @param run.optim
+#' @param x \code{n x 1} numeric vector of observed covariate vector
+#' @param y \code{n x 1} numeric vector of observed response vector
+#' @param L A scalar for the quadratic parameter. If not specified, L is
+#'  data-adaptively selected via optimization or cross-validation
+#' @param run.optim A Boolean parameter for selecting optimization or
+#'  cross-validation. If TRUE, it performs optimization. Default is TRUE.
 #' @param ...
-#'
-#' @return
+#' @return An object with S3 class \code{cvx.quad.est}.
 #' @export
 #'
 #' @examples
@@ -18,21 +22,21 @@ cvx.quad.est <- function(x, y, L=NULL, run.optim=TRUE, ...){
   UseMethod("cvx.quad.est")
 }
 
-
 #' Helper function for fitting convex (or concave) function
 .fit.cvx.reg <- function(x, y, L){
+  # Find convex LSE.
   cvx_final <- simest::cvx.lse.reg(x, y - L*x^2)
   cvx.risk <- mean((y - cvx_final$fit.values - L*x^2)^2)
+  # Find concave LSE.
   concave_final <- simest::cvx.lse.reg(x, -(y - L*x^2))
   concave.risk <- mean((y + concave_final$fit.values - L*x^2)^2)
+  # pick estimator with smaller empirical risk.
   if(cvx.risk < concave.risk){
     cvx_model <- cvx_final; risk <- cvx.risk; convex <- TRUE
-  }
-  else{
+  }else{
     cvx_model <- concave_final; risk <- concave.risk; convex <- FALSE
   }
-  res <- list(reg=cvx_model, risk=risk, convex=convex)
-  return(res)
+  return(list(reg=cvx_model, risk=risk, convex=convex))
 }
 
 #' Helper function for performing CV for the quadratic parameter
@@ -48,19 +52,19 @@ cvx.quad.est <- function(x, y, L=NULL, run.optim=TRUE, ...){
     reg.tr <- .fit.cvx.reg(x.train, y.train, L=l)
     if(reg.tr$convex){
       y.hat <- predict(reg.tr$reg, newdata=x.val) + l * x.val^2
-    }
-    else{
+    }else{
       y.hat <- -predict(reg.tr$reg, newdata=x.val) + l * x.val^2
     }
     mean((y.val-y.hat)^2)
   }
-  opt.res <- optimise(sim.one, lower = L0, upper = (100+L0)*log(n),
-                      maximum = FALSE)
-  list(l.opt = opt.res$minimum, risk =opt.res$objective)
+  val.risk <- sapply(L.list, sim.one); idx <- which.min(val.risk)
+  # return L with the smallest validation error
+  return(list(l.opt = L.list[idx], risk =val.risk[idx]))
 }
 
 #' Helper function for running optimizer for the quadratic parameter
 .run.optimizer.cvx <- function(x, y, L0=0){
+  # split data into train/validation
   n <- length(x); learn.n <- as.integer(n-sqrt(n))
   train.id <- sort(sample(seq_len(n), size =learn.n))
   x.train <- x[train.id]; x.val <- x[-train.id]
@@ -69,27 +73,29 @@ cvx.quad.est <- function(x, y, L=NULL, run.optim=TRUE, ...){
     reg.tr <- .fit.cvx.reg(x.train, y.train, L=l)
     if(reg.tr$convex){
       y.hat <- predict(reg.tr$reg, newdata=x.val) + l * x.val^2
-    }
-    else{
+    }else{
       y.hat <- -predict(reg.tr$reg, newdata=x.val) + l * x.val^2
     }
     mean((y.val-y.hat)^2)
   }
+  # run optimizer to find the best L parameter
   opt.res <- optimise(sim.one, lower = L0, upper = (100+L0)*log(n),
                       maximum = FALSE)
-  list(l.opt = opt.res$minimum, risk =opt.res$objective)
+  # return L with the smallest validation error
+  return(list(l.opt = opt.res$minimum, risk =opt.res$objective))
 }
 
 
-#' Title
+#' Nonparametric LSE for twice differentiable  function
 #'
-#' @param x
-#' @param y
-#' @param L
-#' @param run.optim
-#' @param ...
-#'
-#' @return
+#' @param x \code{n x 1} numeric vector of observed covariate vector
+#' @param y \code{n x 1} numeric vector of observed response vector
+#' @param L A scalar for the quadratic parameter. If not specified, L is
+#'  data-adaptively selected via optimization or cross-validation
+#' @param run.optim A Boolean parameter for selecting optimization or
+#'  cross-validation. If TRUE, it performs optimization. Default is TRUE.
+#' @param ... Additional control parameters
+#' @return An object with S3 class \code{cvx.quad.est}.
 #' @export
 #'
 #' @examples
@@ -100,16 +106,14 @@ cvx.quad.est.default <- function(x, y, L=NULL, run.optim=TRUE, ...){
   # TODO: check input format
   if(run.optim){
     param.selector <- .run.optimizer.cvx; optimizer <- "optimise"
-  }
-  else{
+  }else{
     param.selector <- .run.cross.validation.cvx; optimizer <- "CV"
   }
   if(is.null(L)){
     L <- param.selector(x, y)$l.opt
     est.res <- .fit.cvx.reg(x, y, L)
     cvx_model <- est.res$reg; risk <- est.res$risk; convex=est.res$convex
-  }
-  else{
+  }else{
     est.res <- .fit.cvx.reg(x, y, L)
     cvx_model <- est.res$reg; risk <- est.res$risk; convex=est.res$convex
   }
@@ -117,23 +121,21 @@ cvx.quad.est.default <- function(x, y, L=NULL, run.optim=TRUE, ...){
               g=cvx_model, risk=risk,
               convex=convex, optimizer = optimizer)
   res$call <- match.call()
-
   class(res) <- "cvx.quad.est"
   return(res)
 }
 
-#' Title
+#' Inherit print function for \code{cvx.quad.est}
 #'
-#' @param obj
-#'
-#' @return
+#' @param obj An object with S3 class \code{iso.lin.est}.
+#' @param ... Additional control parameter
 #' @export
 #'
 #' @examples
 #' n <- 100; x <- runif(n, -1, 1); y <- sin(x*4) + rnorm(n, 0, 0.1)
 #' res.est <- cvx.quad.est(x, y)
 #' print(res.est)
-print.cvx.quad.est <- function(obj){
+print.cvx.quad.est <- function(obj, ...){
   cat("Call:\n")
   print(obj$call)
   cat("Empirical Risk:\n")
@@ -146,14 +148,14 @@ print.cvx.quad.est <- function(obj){
   print(obj$optimizer)
 }
 
-#' Title
+#' Inherit predict function for \code{cvx.quad.est}
 #'
-#' @param obj
-#' @param newdata
+#' @param obj An object with S3 class \code{cvx.quad.est}.
+#' @param newdata \code{n x 1} numeric vector of new covariate vector. If not
+#'  specified, use the covariate vector from training.
+#' @return \code{n x 1} numeric vector of predicted values.
 #'
-#' @return
 #' @export
-#'
 #' @examples
 #' n <- 100; x <- runif(n, -1, 1); y <- sin(x*4) + rnorm(n, 0, 0.1)
 #' res.est <- cvx.quad.est(x, y)
@@ -163,12 +165,10 @@ predict.cvx.quad.est <- function(object, newdata=NULL){
   if(is.null(newdata)){
     newdata <- object$x
   }
-  idx <- order(newdata); newdata <- newdata[idx]
   if(object$convex){
     y.hat <- predict(object$g, newdata=newdata) + object$l.value * newdata^2
-  }
-  else{
+  }else{
     y.hat <- -predict(object$g, newdata=newdata) + object$l.value * newdata^2
   }
-  return(y.hat[order(idx)])
+  return(y.hat)
 }
--- R/iso.lin.est.R
+++ R/iso.lin.est.R
@@ -1,16 +1,17 @@
-# Estimating L-Lipschitz function via isotonic regression + linear function
+# Estimating L-Lipschitz function via isotonic regression + linear function.
 
 #' Nonparametric LSE for Lipschitz function
 #'
-#' @param x
-#' @param y
-#' @param L
-#' @param run.optim
-#' @param ...
+#' @param x \code{n x 1} numeric vector of observed covariate vector
+#' @param y \code{n x 1} numeric vector of observed response vector
+#' @param L A scalar for the linear parameter. If not specified, L is
+#'  data-adaptively selected via optimization or cross-validation
+#' @param run.optim A Boolean parameter for selecting optimization or
+#'  cross-validation. If TRUE, it performs optimization. Default is TRUE.
+#' @param ... Additional control parameters
+#' @return An object with S3 class \code{iso.lin.est}.
 #'
-#' @return
 #' @export
-#'
 #' @examples
 #' n <- 100; x <- runif(n, -1, 1); y <- sin(x*4) + rnorm(n, 0, 0.1)
 #' res.est <- iso.lin.est(x, y)
@@ -24,13 +25,13 @@ iso.lin.est <- function(x, y, L=NULL, run.optim=TRUE, ...){
 .fit.iso.reg <- function(x, y, L){
   # Use the min-max formula to find non-decreasing LSE.
   iso_final.incr <- Iso::pava(y - L*x)
-  iso_model.incr <- stepfun(x = x, y = c(iso_final.incr[1], iso_final.incr))
+  iso_model.incr <- stepfun(x=x, y=c(iso_final.incr[1], iso_final.incr))
   incr.risk <- mean((y - iso_model.incr(x) - L*x)^2)
   # Use the min-max formula to find non-increasing LSE.
   iso_final.decr <- Iso::pava(y - L*x, decreasing = TRUE)
   iso_model.decr <- stepfun(x = x, y = c(iso_final.decr[1], iso_final.decr))
   decr.risk <- mean((y - iso_model.decr(x) - L*x)^2)
-  # pick function with smaller empirical risk.
+  # pick estimator with smaller empirical risk.
   if(decr.risk < incr.risk){
     iso_model <- iso_model.decr; risk <- decr.risk
   }
@@ -56,14 +57,14 @@ iso.lin.est <- function(x, y, L=NULL, run.optim=TRUE, ...){
   }
   val.risk <- sapply(L.list, sim.one); idx <- which.min(val.risk)
   # return L with the smallest validation error
-  return(list(l.opt = L.list[idx], risk =val.risk[idx]))
+  return(list(l.opt=L.list[idx], risk=val.risk[idx]))
 }
 
 #' Helper function for running optimizer for the linear parameter
 .run.optimizer.iso <- function(x, y, L0=0){
   # split data into train/validation
   n <- length(x); learn.n <- as.integer(n-sqrt(n))
-  train.id <- sort(sample(seq_len(n), size =learn.n))
+  train.id <- sort(sample(seq_len(n), size=learn.n))
   x.train <- x[train.id]; x.val <- x[-train.id]
   y.train <- y[train.id]; y.val <- y[-train.id]
   sim.one <- function(l){
@@ -72,23 +73,23 @@ iso.lin.est <- function(x, y, L=NULL, run.optim=TRUE, ...){
     mean((y.val-y.hat)^2)
   }
   # run optimizer to find the best L parameter
-  opt.res <- optimise(sim.one, lower = L0, upper = (L0+1)*log(n),
+  opt.res <- optimise(sim.one, lower=L0, upper=(L0+1)*log(n),
                       maximum = FALSE)
   # return L with the smallest validation error
-  return(list(l.opt = opt.res$minimum, risk =opt.res$objective))
+  return(list(l.opt=opt.res$minimum, risk=opt.res$objective))
 }
 
-#' Title
+#' Nonparametric LSE for Lipschitz function
 #'
-#' @param x
-#' @param y
-#' @param L
-#' @param run.optim
-#' @param ...
-#'
-#' @return
+#' @param x \code{n x 1} numeric vector of observed covariate vector
+#' @param y \code{n x 1} numeric vector of observed response vector
+#' @param L A scalar for the linear parameter. If not specified, L is
+#'  data-adaptively selected via optimization or cross-validation
+#' @param run.optim A Boolean parameter for selecting optimization or
+#'  cross-validation. If TRUE, it performs optimization. Default is TRUE.
+#' @param ... Additional control parameters
+#' @return An object with S3 class \code{iso.lin.est}.
 #' @export
-#'
 #' @examples
 #' n <- 100
 #' x <- runif(n, -1, 1)
@@ -100,8 +101,7 @@ iso.lin.est.default <- function(x, y, L=NULL, run.optim=TRUE, ...){
   # select parameter selection procedure
   if(run.optim){
     param.selector <- .run.optimizer.iso; optimizer <- "optimise"
-  }
-  else{
+  }else{
     param.selector <- .run.cross.validation.iso; optimizer <- "CV"
   }
   # run parameter selection if L is not specified
@@ -111,25 +111,24 @@ iso.lin.est.default <- function(x, y, L=NULL, run.optim=TRUE, ...){
   # fit isotonic regression
   est.res <- .fit.iso.reg(x, y, L)
   iso_model <- est.res$reg; risk <- est.res$risk
-  res <- list(x.values = x[order(idx)], y.values = y[order(idx)], l.value=L,
-              g=iso_model, risk=risk, optimizer = optimizer)
+  res <- list(x.values=x[order(idx)], y.values=y[order(idx)], l.value=L,
+              g=iso_model, risk=risk, optimizer=optimizer)
   res$call <- match.call()
   class(res) <- "iso.lin.est"
   return(res)
 }
 
-#' Title
+#' Inherit print function for \code{iso.lin.est}
 #'
-#' @param obj
+#' @param obj An object with S3 class \code{iso.lin.est}.
+#' @param ... Additional control parameter
 #'
-#' @return
 #' @export
-#'
 #' @examples
 #' n <- 100; x <- runif(n, -1, 1); y <- sin(x*4) + rnorm(n, 0, 0.1)
 #' res.est <- iso.lin.est(x, y)
 #' print(res.est)
-print.iso.lin.est <- function(obj){
+print.iso.lin.est <- function(obj, ...){
   cat("Call:\n")
   print(obj$call)
   cat("Empirical Risk:\n")
@@ -140,14 +139,14 @@ print.iso.lin.est <- function(obj){
   print(obj$optimizer)
 }
 
-#' Title
+#' Inherit predict function for \code{iso.lin.est}
 #'
-#' @param obj
-#' @param newdata
+#' @param obj An object with S3 class \code{iso.lin.est}.
+#' @param newdata \code{n x 1} numeric vector of new covariate vector. If not
+#'  specified, use the covariate vector from training.
+#' @return \code{n x 1} numeric vector of predicted values.
 #'
-#' @return
 #' @export
-#'
 #' @examples
 #' n <- 100; x <- runif(n, -1, 1); y <- sin(x*4) + rnorm(n, 0, 0.1)
 #' res.est <- iso.lin.est(x, y)
@@ -158,5 +157,5 @@ predict.iso.lin.est <- function(obj, newdata=NULL){
   if(is.null(newdata)){
     newdata <- obj$x
   }
-  return(obj$g(newdata) + obj$l.value * newdata)
+  return(obj$g(newdata) + obj$l.value*newdata)
 }
